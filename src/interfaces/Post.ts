25863333897444400a2dd0143e1787c996cb11dd
Matheus Duarte <matheus.duartesilva42@gmail.com>
2024-08-13 19:31:41 -0300

    feat: adicionando tipagem dos Posts e Tags

---
+++ src/interfaces/Post.ts
@@ -0,0 +1,11 @@
+import { Tag } from "./Tag";
+
+export interface Post {
+  title: string;
+  slug: { current: string };
+  publishedAt: string;
+  excerpt: string;
+  body: any;
+  tags: Array<Tag>;
+  _id: string;
+}
