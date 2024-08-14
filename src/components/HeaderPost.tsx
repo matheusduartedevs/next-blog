870a8207e40b12dc1e81acafa685bc9a08720e9b
Matheus Duarte <matheus.duartesilva42@gmail.com>
2024-08-14 19:21:56 -0300

    feat: adicionando tags na header

--- src/components/HeaderPost.tsx
+++ src/components/HeaderPost.tsx
@@ -1,13 +1,22 @@
+import Link from "next/link";
+
 type HeaderPostsProps = {
   title: string;
+  tags?: boolean;
 };
 
-const HeaderPosts = ({ title = "" }: HeaderPostsProps) => {
+const HeaderPosts = ({ title = "", tags = false }: HeaderPostsProps) => {
   return (
     <header className="py-14 px-4 mb-12 text-center border-b border-purple-900">
       <h2 className="uppercase text-2xl mx-auto max-w-2xl font-bold">
         {title}
       </h2>
+
+      {tags && (
+        <div className="text-xs mt-2 hover:text-purple-500">
+          <Link href="/tag">#tags</Link>
+        </div>
+      )}
     </header>
   );
 };
